9e6616c7381afdf71a54ad85dca6fc925b9c5ba0
pingnak@gmail.com <pingnak@gmail.com@44f8dc7b-d01c-faae-8da7-85d0e9518aa4>
2013-08-25 03:40:00 +0000

    Compress it down to one index.html file, containing all of the indexes.  Now if you want, you can build indexes for other trees, but you don't have to.
    Displays a slightly modified, color coded folder hierarchy based on the folders' parents.  
    
    File lists expand from and collapse into folders.
    
    Plays continuously within a folder, then stops without crawling out of it.

--- webify.sh
+++ webify.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 #
-# Script to generate web page tree from video collection
+# Script to generate a single web file of all videos
 #
 
 # Default (no parameter) search path for video files
 CONTENT_ROOT="/Volumes/video"
 
-# Top level folder navigator
-WEBIFY_MAIN_INDEX="TOC.html"
-
-# Each folder gets an index.html, but you can rename it if it conflicts
-WEBIFY_INDEX="index.html"
+# Adds 'TOC.html' in root of video tree, lists all folders
+WEBIFY_MAIN_INDEX="index.html"
 
 # Get root of script, where we assume the template folder will be
 SCRIPT_ROOT=`dirname "$0"`
@@ -31,30 +28,18 @@ INDEX_FOLDER=`cat $SCRIPT_TEMPLATES/index_folder.html`
 if test -d "$1" ; then 
 	CONTENT_ROOT="$1"
 else
-    if test "$1" = "-clean" ; then
-        rm "$CONTENT_ROOT/$WEBIFY_MAIN_INDEX"
-        find -s "$CONTENT_ROOT" -name '*.mp4' -o -name '*.ogg' -o -name '*.webm' | while read i ; do dirname "$i" ; done | sort -u | while read folder_path ; 
-        do
-            rm "$folder_path/$WEBIFY_INDEX"
-        done
-        exit 0
-    else 
-        if test -z "$1" ; then
-            echo "Using default: $CONTENT_ROOT"
-        else
-            echo
-            echo "Invalid parameter."
-            echo
-            echo "$0"
-            echo "    Generate video indexes/players in default location set in 'CONTENT_ROOT'."
-            echo
-            echo "$0 PATH_TO_VIDEO"
-            echo "    Change path to 'PATH_TO_VIDEO', and do the job."
-            echo
-            echo "$0 -clean"
-            echo "    Remove up what this did."
-            exit 1
-        fi
+    if test -z "$1" ; then
+        echo "Using default: $CONTENT_ROOT"
+    else
+        echo
+        echo "Invalid parameter."
+        echo
+        echo "$0"
+        echo "    Generate video indexes/players in default location set in 'CONTENT_ROOT'."
+        echo
+        echo "$0 PATH_TO_VIDEO"
+        echo "    Change path to 'PATH_TO_VIDEO', and do the job."
+        exit 1
     fi
 fi
 
@@ -62,70 +47,65 @@ set -o errexit	# Stop running the script if an error occurs
 set -o nounset	# Stop running the script if a variable isn't set
 #set -o verbose	# Echo every command
 
-
-# lovingly stolen from https://gist.github.com/1573996
-relpath() {
-    local from="$1" to="$2"
-    from=${from//\/\//\/}
-    from=${from%/}
-    IFS=/
-    dirs=(${from#/})
-    for to; do
-        to=${to//\/\//\/}
-        to=${to%/}
-        local commonPrefix=/ d=
-        for d in "${dirs[@]}"; do
-            case "$to/" in "$commonPrefix$d/"*) ;;
-            *) break;;
-            esac
-            commonPrefix+="$d/"
-        done
-        local ancestor="${from#${commonPrefix%/}}"
-        ancestor=${ancestor//[^\/]/}
-        ancestor=${ancestor//\//..\/}
-    done
-    echo "$ancestor${to#$commonPrefix}"
-}
-
 #
 # Now that the globals and resources are set up, call the folder webifier
 #
 main_index="$CONTENT_ROOT/$WEBIFY_MAIN_INDEX"
 
+folder_start=`basename "$CONTENT_ROOT"`
+
 # Generate top portion of index file
 cat $INDEX_TOPMOST > "$main_index"
 cat $INDEX_CSS >> "$main_index"
-cat "$SCRIPT_TEMPLATES/index_main_prolog.html" >> "$main_index"
+cat $INDEX_PROLOG | sed \-e "s@TITLE_TEXT@$folder_start@g" >> "$main_index"
+
+# If we had content in the root of the tree, we need to keep the rest aligned
+needed_root=
 
 # Find all video files, then get the paths, then sort to unique, to build a 
 # list of folers to process.
 echo
-echo On LAN, initial find may take a little time...
-find -s "$CONTENT_ROOT" -name '*.mp4' -o -name '*.ogg' -o -name '*.webm' | while read i ; do dirname "$i" ; done | sort -u | while read folder_path ; 
-do 
-    echo "$folder_path"
-    folder_relative=`relpath "$CONTENT_ROOT" "$folder_path"`
-    folder_name=${folder_path##*/}
-    folder_name_noext=${folder_name%.*}
+echo On a LAN with a lot of files, initial find may take a little time...
+pushd "$CONTENT_ROOT" > /dev/null
+find -s . -name '*.mp4' -o -name '*.ogg' -o -name '*.webm' | while read i ; do dirname "$i" ; done | sort -u | while read folder_relative ; 
+do
+    echo $folder_relative
+    # Lookup 'BASH parameter expansion', to decode this stuff
+    folder_name=`basename "$folder_relative"`
+    folder_path="$CONTENT_ROOT${folder_relative#.}"
+    folder_parent=`dirname "$folder_relative"`
+    if test "." = "$folder_name" ; then
+        folder_name="$folder_start"
+        needed_root="$folder_start/"
+        folder_parent=
+    else
+        if test "." = "$folder_parent" ; then
+            folder_parent="$needed_root"
+        else
+            folder_parent="$needed_root${folder_parent:2}/"
+        fi
+    fi
     
-    index_file="$folder_path/$WEBIFY_INDEX"
+    # Parse folder name depth to indent?
+    echo $INDEX_FOLDER | sed \
+        -e "s@FOLDER_PARENT@$folder_parent@g" \
+        -e "s@FOLDER_TITLE@$folder_name@g" >> "$main_index"
 
-    # Generate top portion of index file
-    cat $INDEX_TOPMOST > "$index_file"
-    cat $INDEX_CSS >> "$index_file"
-    cat $INDEX_PROLOG | sed -e "s@TITLE_TEXT@$folder_name_noext@g" >> "$index_file"
-
-    pushd "$folder_path" > /dev/null
-    find -s . -depth 1 -name '*.mp4' -o -name '*.ogg' -o -name '*.webm' | while read movie_path ; 
+    # Now drop in a collapsed tree of titles for the current folder entry
+    echo '<div style="display:none;" id="titles">' >> "$main_index"
+    find -s "$folder_relative" -depth 1 -name '*.mp4' -o -name '*.ogg' -o -name '*.webm' | while read movie_path ; 
     do 
-        echo "    $movie_path"
+        echo "$movie_path"
         filename=${movie_path##*/}
         filename_noext=${filename%.*}
-        filename_extension=${filename##*.}
-        filename_jpeg=${filename%.*}.jpg
+        filename_jpeg=${movie_path%.*}.jpg
+
         # Generate a thumbnail (if it doesn't already exist)
         # https://code.google.com/p/ffmpegthumbnailer/
         # Puts it along-side the movies, since that's the way my DLNA server takes 'em.
+        # If you think that's 'messy', you can dump all the thumbnails into a different
+        # folder by tinkering with the 'filename_jpeg', and making that folder.
+        # e.g. filename_jpeg=$CONTENT_ROOT/thumbnails/$filename_noext.jpg
         if test ! -f "$filename_jpeg"; then
             echo ffmpegthumbnailer "$movie_path" 
             ffmpegthumbnailer -f -t18 -s256 -i "$movie_path" -o "$filename_jpeg"
@@ -134,19 +114,11 @@ do
         # Spit out a line of table of contents
         echo $INDEX_LINE | \
             sed -e "s@VIDEO_IMAGE@$filename_jpeg@g" \
-            -e "s@VIDEO_PATH@$filename@g" \
-            -e "s@MOVIE_TITLE@$filename_noext@g" >> "$index_file"
+            -e "s@VIDEO_PATH@$movie_path@g" \
+            -e "s@MOVIE_TITLE@$filename_noext@g" >> "$main_index"
     done
-    popd > /dev/null
-
-    # Finish index file
-    cat $INDEX_EPILOG >> "$index_file"
-
-    # Parse folder name depth to indent?
-    echo $INDEX_FOLDER | sed \
-        -e "s@FOLDER_PATH@$folder_relative/$WEBIFY_INDEX@g" \
-        -e "s@FOLDER_TITLE@$folder_relative@g" >> "$main_index"
-    
+    echo '</div>' >> "$main_index"
 done
+popd > /dev/null
 
-cat "$SCRIPT_TEMPLATES/index_main_epilog.html" >> "$main_index"
+cat $INDEX_EPILOG >> "$main_index"
