a40469805a3f7b473efa32707a4a57a5e1295560
pingnak <pingnak@gmail.com>
2015-05-16 13:08:31 -0700

    Add hierarchical indent to ‘Folder Index’ in python scripts
    While not identical to the AIR app’s behavior of constructing
    sub-folder links, it is very usable in this state.  I wonder whether I
    should recursively call the index construction on all found folders,
    and be done with it?
    
    Through the miracle of testing, discovered I was emitting backslashes
    in web URIs in the python scripts.  Fixed that.
    
    Add ‘<meta charset="UTF-8”>’, where it was missing in index templates.

--- jukeboxscript.py
+++ jukeboxscript.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# shebang for unix-like shells... Windows users type 'python jukeboxscript.py'
 
 #
 # The python version.
@@ -20,7 +21,7 @@ from xml.sax.saxutils import escape
 root_dir = os.path.normpath(sys.argv[1])
 need_jpeg= [];
 
-# What to call the video player files
+# What to call the media player files
 WEBIFY_PLAYER_INDEX="Jukebox.html"
 
 # What to call the index/table of contents
@@ -65,100 +66,151 @@ with open (INDEX_TEMPLATE_FILE, "r") as myfile:
 
 # Folder index list (left side of template file)
 INDEX_SMALL_MATCH = re.search('<!--INDEX_SMALL(.*?)-->', INDEX_TEMPLATE, re.MULTILINE|re.DOTALL)
-INDEX_SMALL = INDEX_SMALL_MATCH.group(1);
+INDEX_SMALL = INDEX_SMALL_MATCH.group(1)
 
 # Folder index (right side of template file)
 INDEX_TOC_FOLDER_MATCH=re.search('<!--INDEX_TOC_FOLDER(.*?)-->', INDEX_TEMPLATE, re.MULTILINE|re.DOTALL)
-INDEX_TOC_FOLDER = INDEX_TOC_FOLDER_MATCH.group(1);
+INDEX_TOC_FOLDER = INDEX_TOC_FOLDER_MATCH.group(1)
 
 # Play file index   
 INDEX_TOC_FILE_MATCH=re.search('<!--INDEX_TOC_FILE(.*?)-->', INDEX_TEMPLATE, re.MULTILINE|re.DOTALL)
-INDEX_TOC_FILE = INDEX_TOC_FILE_MATCH.group(1);
+INDEX_TOC_FILE = INDEX_TOC_FILE_MATCH.group(1)
 
-print( "Crawling folders in %s...\n" % (root_dir) )
+# File/folder left padding
+LEFT_PADDING = 4
+
+# Offset for folder depths in TOC file
+FOLDER_DEPTH = 24
+
+
+print( "\nCrawling folders in %s...\n" % (root_dir) )
 
 #
 # Iterate folders & files
 #
-index_toc_small=""
+all_paths_with_media=[]
+total_media_count = 0
+index_toc_small=[]
 index_toc=""
 
-
-
 for root, dirs, files in sorted(os.walk(root_dir)):
-    files.sort();
+    files.sort()
 
+    # Bake some details about this folder
+    folder_path, folder_name = os.path.split(root)
+    folder_relative = os.path.relpath(root,root_dir)
+    folder_curr = os.path.relpath(root,root_dir)
+    folder_curr_escaped = os.path.join(folder_curr,WEBIFY_PLAYER_INDEX)
+    folder_curr_escaped = urllib.quote(folder_curr_escaped.replace('\\', '/')) # Fix any Windows backslashes
+    folder_name_escaped = escape(folder_name)
+    folder_depth = len(os.path.relpath(root, root_dir).split(os.sep))-1
+    if '.' != folder_curr:
+        folder_depth = folder_depth + 1;
+    folder_depth = LEFT_PADDING + (folder_depth*FOLDER_DEPTH)
+
+    # How many media files in this folder?
     totalFiles = 0
     for relPath in files:
         dummy,extCurr = os.path.splitext(relPath)
         if extCurr.lower() in FILE_TYPES:
             totalFiles = totalFiles + 1
 
+    # Record folder for left box index/TOC
+    small_output = str(INDEX_SMALL)
+    small_output = small_output.replace( 'FOLDER_TITLE', folder_name_escaped)
+    small_output_style = 'padding-left:' + str(folder_depth) + 'px;'
+
+    # A folder with content to play
     if 0 != totalFiles:
+
+        # Record live link folder for left box index/TOC
+        all_paths_with_media.append(root)
+        small_output = small_output.replace( 'FOLDER_PATH',  folder_curr_escaped)
+        small_output = small_output.replace( 'FOLDER_STYLE', small_output_style )
+        index_toc_small.append( (root,small_output) );
+
         playlist=""
 
-        folder_path, folder_name = os.path.split(root);
-        folder_relative = os.path.relpath(root,root_dir);
-        folder_curr = os.path.relpath(root,root_dir);
-        folder_curr_escaped = urllib.quote(os.path.join(folder_curr,WEBIFY_PLAYER_INDEX))
-        folder_name_escaped = escape(folder_name)
         print folder_relative
-        sys.stdout.flush();
+        sys.stdout.flush()
 
-        # Record folder for left box index/TOC
-        output = str(INDEX_SMALL);
-        output = output.replace( 'FOLDER_TITLE', folder_name_escaped);
-        output = output.replace( 'FOLDER_PATH',  folder_curr_escaped);
-        output = output.replace( 'FOLDER_STYLE', '');
-        index_toc_small = index_toc_small + output;
-    
         # Record folder for big folder+file TOC
-        output = str(INDEX_TOC_FOLDER);
-        output = output.replace( 'FOLDER_TITLE', folder_name_escaped);
-        output = output.replace( 'FOLDER_PATH',  folder_curr_escaped);
-        output = output.replace( 'FOLDER_STYLE', '');
-        index_toc = index_toc + output;
+        output = str(INDEX_TOC_FOLDER)
+        output = output.replace( 'FOLDER_TITLE', folder_name_escaped)
+        output = output.replace( 'FOLDER_PATH',  folder_curr_escaped)
+        output = output.replace( 'FOLDER_STYLE', '')
+        index_toc = index_toc + output
 
         for relPath in files:
+
+            # Skip files that aren't playable
             fileName, fileExtension = os.path.splitext(relPath)
             fullPath = os.path.join(root, fileName) + fileExtension
-            # There should be a jpeg file in folder; if not, add to a list to make it
             dummy,extCurr = os.path.splitext(relPath)
             if extCurr.lower() in FILE_TYPES:
-                media_path = os.path.relpath(fullPath,root);
-                media_path_escaped=urllib.quote(media_path)
+                # Keep track for trivia
+                total_media_count = total_media_count + 1;
+                
+                # Bake some details about this file
+                media_path = os.path.relpath(fullPath,root)
+                media_path_escaped = urllib.quote(media_path.replace('\\', '/')) # Fix any Windows backslashes
                 filename_title_escaped=escape(fileName)
-                pathcurr=str(INDEX_FILE);
-
-                # Emit elements for video player
-                pathcurr = pathcurr.replace( 'MEDIA_PATH', media_path_escaped);
-                pathcurr = pathcurr.replace( 'FILE_STYLE', '');
-                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped);
-                playlist = playlist + pathcurr;
+                
+                # Emit elements for media player
+                pathcurr=str(INDEX_FILE)
+                pathcurr = pathcurr.replace( 'MEDIA_PATH', media_path_escaped)
+                pathcurr = pathcurr.replace( 'FILE_STYLE', '')
+                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped)
+                playlist = playlist + pathcurr
 
                 # Emit elements for index navigation player
                 pathcurr = str(INDEX_TOC_FILE)
-                pathcurr = pathcurr.replace( 'MEDIA_PATH', urllib.quote(os.path.join(folder_curr,WEBIFY_PLAYER_INDEX))+'?'+media_path_escaped);
-                pathcurr = pathcurr.replace( 'FILE_STYLE', '');
-                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped);
-                index_toc = index_toc + pathcurr;
-
+                pathcurr_escaped = os.path.join(folder_curr,WEBIFY_PLAYER_INDEX).replace('\\', '/')
+                pathcurr_escaped = urllib.quote(pathcurr_escaped)+'?'+media_path_escaped
+                pathcurr = pathcurr.replace( 'MEDIA_PATH', pathcurr_escaped)
+                pathcurr = pathcurr.replace( 'FILE_STYLE', '')
+                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped)
+                index_toc = index_toc + pathcurr
+                
         # Manufacture a VideoPlayer.html for folder
-        folder_path, folder_name=os.path.split(root);
-        output = str(PLAYER_TEMPLATE);
-        output = output.replace( 'TITLE_TEXT', escape(folder_name));
-        output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE);
-        output = output.replace( '<!--INDEXES_HERE-->', playlist);
+        folder_path, folder_name=os.path.split(root)
+        output = str(PLAYER_TEMPLATE)
+        output = output.replace( 'TITLE_TEXT', escape(folder_name))
+        output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE)
+        output = output.replace( '<!--INDEXES_HERE-->', playlist)
         with open(os.path.join(root,WEBIFY_PLAYER_INDEX), "w") as text_file:
             text_file.write(output)
+            
+    else:
+        # Record dead link folder for left box index/TOC
+        small_output = small_output.replace( 'FOLDER_PATH', '')
+        small_output = small_output.replace( 'FOLDER_STYLE', small_output_style + ' pointer-events: none; opacity: 0.75;' )
+        index_toc_small.append( (root,small_output) );
 
-# Manufacture an index.html for entire run
-sys.stdout.flush();
-print "Building index..."
-output = str(INDEX_TEMPLATE);
-output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE);
-output = output.replace( '<!--FOLDERS_HERE-->', index_toc_small);
-output = output.replace( '<!--INDEXES_HERE-->', index_toc);
-with open(os.path.join(root_dir,WEBIFY_INDEX), "w") as text_file:
-    text_file.write(output)
 
+# Manufacture index.html for entire run of folders, if there were any
+if 0 != len(all_paths_with_media):
+    print "\nBuilding index..."
+    sys.stdout.flush()
+    
+    # Build index file
+    output = str(INDEX_TEMPLATE)
+    output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE)
+    output = output.replace( '<!--INDEXES_HERE-->', index_toc)
+    
+    # Add the small index list; Only emit index paths that lead to media 
+    small_indexes=''
+    for indexPath, item in sorted(index_toc_small):
+        for apath in all_paths_with_media:
+            if 0 == apath.find(indexPath):
+                small_indexes = small_indexes + item + '\n'
+                break;
+    output = output.replace( '<!--FOLDERS_HERE-->', small_indexes)
+    
+    # Write index file
+    with open(os.path.join(root_dir,WEBIFY_INDEX), "w") as text_file:
+        text_file.write(output)
+            
+    print "\nMade %d folders with %d files.\n" %(len(all_paths_with_media),total_media_count)
+else:
+    print "\nNo media found.  Nothing written.\n"
--- videowebscript.py
+++ videowebscript.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# shebang for unix-like shells... Windows users type 'python videowebscript.py'
 
 #
 # The python version.
@@ -17,7 +18,7 @@ from xml.sax.saxutils import escape
 import urllib
 
 root_dir = os.path.normpath(sys.argv[1])
-need_jpeg= [];
+need_jpeg= []
 
 # What to call the video player files
 WEBIFY_PLAYER_INDEX="VideoPlayer.html"
@@ -77,59 +78,80 @@ with open (INDEX_TEMPLATE_FILE, "r") as myfile:
 
 # Folder index list (left side of template file)
 INDEX_SMALL_MATCH = re.search('<!--INDEX_SMALL(.*?)-->', INDEX_TEMPLATE, re.MULTILINE|re.DOTALL)
-INDEX_SMALL = INDEX_SMALL_MATCH.group(1);
+INDEX_SMALL = INDEX_SMALL_MATCH.group(1)
 
 # Folder index (right side of template file)
 INDEX_TOC_FOLDER_MATCH=re.search('<!--INDEX_TOC_FOLDER(.*?)-->', INDEX_TEMPLATE, re.MULTILINE|re.DOTALL)
-INDEX_TOC_FOLDER = INDEX_TOC_FOLDER_MATCH.group(1);
+INDEX_TOC_FOLDER = INDEX_TOC_FOLDER_MATCH.group(1)
 
 # Play file index   
 INDEX_TOC_FILE_MATCH=re.search('<!--INDEX_TOC_FILE(.*?)-->', INDEX_TEMPLATE, re.MULTILINE|re.DOTALL)
-INDEX_TOC_FILE = INDEX_TOC_FILE_MATCH.group(1);
+INDEX_TOC_FILE = INDEX_TOC_FILE_MATCH.group(1)
 
-print( "Crawling folders in %s...\n" % (root_dir) )
+# File/folder left padding
+LEFT_PADDING = 4
+
+# Offset for folder depths in TOC file
+FOLDER_DEPTH = 24
+
+
+print( "\nCrawling folders in %s...\n" % (root_dir) )
 
 #
 # Iterate folders & files
 #
-index_toc_small=""
+all_paths_with_media=[]
+total_media_count = 0
+index_toc_small=[]
 index_toc=""
 
 for root, dirs, files in sorted(os.walk(root_dir)):
-    files.sort();
+    files.sort()
 
+    # Bake some details about this folder
+    folder_path, folder_name = os.path.split(root)
+    folder_relative = os.path.relpath(root,root_dir)
+    folder_curr = os.path.relpath(root,root_dir)
+    folder_curr_escaped = os.path.join(folder_curr,WEBIFY_PLAYER_INDEX)
+    folder_curr_escaped = urllib.quote(folder_curr_escaped.replace('\\', '/')) # Fix any Windows backslashes
+    folder_name_escaped = escape(folder_name)
+    folder_depth = len(os.path.relpath(root, root_dir).split(os.sep))-1
+    if '.' != folder_curr:
+        folder_depth = folder_depth + 1;
+    folder_depth = LEFT_PADDING + (folder_depth*FOLDER_DEPTH)
+
+    # How many media files in this folder?
     totalFiles = 0
     for relPath in files:
         dummy,extCurr = os.path.splitext(relPath)
         if extCurr.lower() in FILE_TYPES:
             totalFiles = totalFiles + 1
 
+    # Record folder for left box index/TOC
+    small_output = str(INDEX_SMALL)
+    small_output = small_output.replace( 'FOLDER_TITLE', folder_name_escaped)
+    small_output_style = 'padding-left:' + str(folder_depth) + 'px;'
+
+    # A folder with content to play
     if 0 != totalFiles:
+
+        # Record live link folder for left box index/TOC
+        all_paths_with_media.append(root)
+        small_output = small_output.replace( 'FOLDER_PATH',  folder_curr_escaped)
+        small_output = small_output.replace( 'FOLDER_STYLE', small_output_style )
+        index_toc_small.append( (root,small_output) );
+
         playlist=""
 
-        # Bake some details about this folder
-        folder_path, folder_name = os.path.split(root);
-        folder_relative = os.path.relpath(root,root_dir);
-        folder_curr = os.path.relpath(root,root_dir);
-        folder_curr_escaped = urllib.quote(os.path.join(folder_curr,WEBIFY_PLAYER_INDEX))
-        folder_name_escaped = escape(folder_name)
-
         print folder_relative
-        sys.stdout.flush();
+        sys.stdout.flush()
 
-        # Record folder for left box index/TOC
-        output = str(INDEX_SMALL);
-        output = output.replace( 'FOLDER_TITLE', folder_name_escaped);
-        output = output.replace( 'FOLDER_PATH',  folder_curr_escaped);
-        output = output.replace( 'FOLDER_STYLE', '');
-        index_toc_small = index_toc_small + output;
-    
         # Record folder for big folder+file TOC
-        output = str(INDEX_TOC_FOLDER);
-        output = output.replace( 'FOLDER_TITLE', folder_name_escaped);
-        output = output.replace( 'FOLDER_PATH',  folder_curr_escaped);
-        output = output.replace( 'FOLDER_STYLE', '');
-        index_toc = index_toc + output;
+        output = str(INDEX_TOC_FOLDER)
+        output = output.replace( 'FOLDER_TITLE', folder_name_escaped)
+        output = output.replace( 'FOLDER_PATH',  folder_curr_escaped)
+        output = output.replace( 'FOLDER_STYLE', '')
+        index_toc = index_toc + output
 
         for relPath in files:
 
@@ -138,61 +160,89 @@ for root, dirs, files in sorted(os.walk(root_dir)):
             fullPath = os.path.join(root, fileName) + fileExtension
             dummy,extCurr = os.path.splitext(relPath)
             if extCurr.lower() in FILE_TYPES:
+                # Keep track for trivia
+                total_media_count = total_media_count + 1;
                 
                 # Bake some details about this file
-                media_path = os.path.relpath(fullPath,root);
-                media_path_escaped=urllib.quote(media_path)
+                media_path = os.path.relpath(fullPath,root)
+                media_path_escaped = urllib.quote(media_path.replace('\\', '/')) # Fix any Windows backslashes
                 filename_title_escaped=escape(fileName)
-                pathcurr=str(INDEX_FILE);
+                pathcurr=str(INDEX_FILE)
                 
                 if HAVE_THUMBNAILER:
                     # We should have a jpg file, named the same as the playable file
                     jpegName = fileName + '.jpg'
-                    jpegPath = os.path.join(root, jpegName);
+                    jpegPath = os.path.join(root, jpegName)
                     filename_jpeg_escaped=urllib.quote(os.path.relpath(jpegPath,root))
                     if jpegName not in files:
                         need_jpeg.append( (fullPath,jpegPath) )
-                    pathcurr=re.sub('MEDIA_IMAGE',filename_jpeg_escaped,pathcurr);
+                    pathcurr=re.sub('MEDIA_IMAGE',filename_jpeg_escaped,pathcurr)
                 else:
                     # We don't have a thumbnailer.
-                    pathcurr=str(INDEX_FILE_NOTHUMB);
+                    pathcurr=str(INDEX_FILE_NOTHUMB)
 
                 # Emit elements for video player
-                pathcurr = pathcurr.replace( 'MEDIA_PATH', media_path_escaped);
-                pathcurr = pathcurr.replace( 'FILE_STYLE', '');
-                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped);
-                playlist = playlist + pathcurr;
+                pathcurr = pathcurr.replace( 'MEDIA_PATH', media_path_escaped)
+                pathcurr = pathcurr.replace( 'FILE_STYLE', '')
+                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped)
+                playlist = playlist + pathcurr
 
                 # Emit elements for index navigation player
                 pathcurr = str(INDEX_TOC_FILE)
-                pathcurr = pathcurr.replace( 'MEDIA_PATH', urllib.quote(os.path.join(folder_curr,WEBIFY_PLAYER_INDEX))+'?'+media_path_escaped);
-                pathcurr = pathcurr.replace( 'FILE_STYLE', '');
-                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped);
-                index_toc = index_toc + pathcurr;
-
+                pathcurr_escaped = os.path.join(folder_curr,WEBIFY_PLAYER_INDEX).replace('\\', '/')
+                pathcurr_escaped = urllib.quote(pathcurr_escaped)+'?'+media_path_escaped
+                pathcurr = pathcurr.replace( 'MEDIA_PATH', pathcurr_escaped)
+                pathcurr = pathcurr.replace( 'FILE_STYLE', '')
+                pathcurr = pathcurr.replace( 'MEDIA_TITLE', filename_title_escaped)
+                index_toc = index_toc + pathcurr
+                
         # Manufacture a VideoPlayer.html for folder
-        folder_path, folder_name=os.path.split(root);
-        output = str(PLAYER_TEMPLATE);
-        output = output.replace( 'TITLE_TEXT', escape(folder_name));
-        output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE);
-        output = output.replace( '<!--INDEXES_HERE-->', playlist);
+        folder_path, folder_name=os.path.split(root)
+        output = str(PLAYER_TEMPLATE)
+        output = output.replace( 'TITLE_TEXT', escape(folder_name))
+        output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE)
+        output = output.replace( '<!--INDEXES_HERE-->', playlist)
         with open(os.path.join(root,WEBIFY_PLAYER_INDEX), "w") as text_file:
             text_file.write(output)
+            
+    else:
+        # Record dead link folder for left box index/TOC
+        small_output = small_output.replace( 'FOLDER_PATH', '')
+        small_output = small_output.replace( 'FOLDER_STYLE', small_output_style + ' pointer-events: none; opacity: 0.75;' )
+        index_toc_small.append( (root,small_output) );
 
-# Manufacture index.html for entire run of folders
-print "Building index..."
-sys.stdout.flush();
-output = str(INDEX_TEMPLATE);
-output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE);
-output = output.replace( '<!--FOLDERS_HERE-->', index_toc_small);
-output = output.replace( '<!--INDEXES_HERE-->', index_toc);
-with open(os.path.join(root_dir,WEBIFY_INDEX), "w") as text_file:
-    text_file.write(output)
 
-# Build thumbnails from files that need them.
-if HAVE_THUMBNAILER:
-    print "Making thumbnails..."
-    sys.stdout.flush();
-    for needs in need_jpeg:
-        print("Making jpeg from: " + needs[0])
-        call([ THUMBNAILER, '-t', str(random.randrange(25, 75))+'%', '-s', str(THUMB_SIZE), '-i', needs[0], '-o', needs[1] ])
+# Manufacture index.html for entire run of folders, if there were any
+if 0 != len(all_paths_with_media):
+    print "\nBuilding index..."
+    sys.stdout.flush()
+    
+    # Build index file
+    output = str(INDEX_TEMPLATE)
+    output = output.replace( '/*INSERT_CSS_HERE*/', CSS_TEMPLATE)
+    output = output.replace( '<!--INDEXES_HERE-->', index_toc)
+    
+    # Add the small index list; Only emit index paths that lead to media 
+    small_indexes=''
+    for indexPath, item in sorted(index_toc_small):
+        for apath in all_paths_with_media:
+            if 0 == apath.find(indexPath):
+                small_indexes = small_indexes + item + '\n'
+                break;
+    output = output.replace( '<!--FOLDERS_HERE-->', small_indexes)
+    
+    # Write index file
+    with open(os.path.join(root_dir,WEBIFY_INDEX), "w") as text_file:
+        text_file.write(output)
+
+    # Build thumbnails from files that need them.
+    if HAVE_THUMBNAILER:
+        print "\nMaking thumbnails..."
+        sys.stdout.flush()
+        for needs in need_jpeg:
+            print("Making jpeg from: " + needs[0])
+            call([ THUMBNAILER, '-t', str(random.randrange(25, 75))+'%', '-s', str(THUMB_SIZE), '-i', needs[0], '-o', needs[1] ])
+            
+    print "\nMade %d folders with %d files.\n" %(len(all_paths_with_media),total_media_count)
+else:
+    print "\nNo media found.  Nothing written.\n"
